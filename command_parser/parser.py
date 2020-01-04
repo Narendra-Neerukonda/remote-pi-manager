a58c668989613adc61b3c551a29e641b085a06e2
Narendra <narendra.klu9@gmail.com>
2020-01-04 10:29:25 +0000

    added responding capability to parser module.

--- command_parser/parser.py
+++ command_parser/parser.py
@@ -3,6 +3,8 @@ import json
 import configparser
 
 from workflows.command_executor import execute_command
+from responder.messaging import sendtext
+
 
 storage_config = configparser.ConfigParser()
 storage_config.read(os.path.join(os.path.dirname(__file__),"..","conf","storage.ini"))
@@ -13,13 +15,17 @@ def parse():
             loaded_line = json.loads(line)
             if 'message' in loaded_line:
 
-                # Code block to execute specific shell commands
+                # Code block to execute specific shell commands. syntax- command: <actual command>
                 if 'text' in loaded_line['message']:
                     if 'command' in loaded_line['message']['text']:
                         command = loaded_line['message']['text'].split(':')
                         if len(command) > 1:
-                            execute_command(command[1])
-
+                            response = execute_command(command[1])
+                            sendtext(
+                                "Responding for command "+str(command[1])+"\n"
+                                + str(response)
+                                )
+            
 
 
 if __name__=="__main__":
