a6274ae693fde66ab8dc6d2aadfcecda280ffd43
Narendra <narendra.klu9@gmail.com>
2020-01-04 17:39:45 +0000

    Modified aria2 command to not use dht control option and exit on completion option.

--- workflows/torrent_downloader.py
+++ workflows/torrent_downloader.py
@@ -8,5 +8,5 @@ storage_config.read(os.path.join(os.path.dirname(__file__),"..","conf","storage.
 
 def start_torrent_data_download(torrent_file):
     
-    download_status = os.popen("aria2c -T"+" "+ torrent_file+" "+"--seed-time=0 --enable-dht=false"+" "+"-d"+" "+storage_config['save_files']['torrents']).read()
+    download_status = os.popen("aria2c -T"+" "+ torrent_file+" "+"--seed-time=0 --file-allocation=none"+" "+"-d"+" "+storage_config['save_files']['torrents']+" "+"--on-download-complete=exit").read()
     return "Download Completed"
