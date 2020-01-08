43ffe5ad12a1a1c3a94acfb878e67223f9c19959
Narendra <narendra.klu9@gmail.com>
2020-01-08 13:35:11 +0000

    Added new code to enable posting imagesto telegram bot.

--- responder/messaging.py
+++ responder/messaging.py
@@ -13,3 +13,14 @@ def sendtext(message):
     response = requests.get("https://api.telegram.org/bot"+auth_config['bot']['token']+"/sendMessage?chat_id="+auth_config['bot']['chat_id']+"&parse_mode=Markdown&text="+message)
 
     return response.json()
+
+
+def send_image(image_location):
+    
+    url = "https://api.telegram.org/bot"+auth_config['bot']['token']+"/sendPhoto"
+
+    up = {'photo':(image_location, open(image_location, 'rb'), "multipart/form-data")}
+    data={'chat_id':auth_config['bot']['chat_id']}
+
+    data = requests.post(url,files=up, data=data)
+    return data.text
